f6d06d101c0e4929fc6b0a7b75b9cff96f630305
Daniel <danspr@wp.pl>
2022-08-22 15:50:57 +0200

    add 10-step_progress_bar

--- 10-step_proggress-bar/index.js
+++ 10-step_proggress-bar/index.js
@@ -1,16 +1,55 @@
-const leftBtnEl = document.querySelector(".fa-left-long");
-const rightBtnEl = document.querySelector(".fa-right-long");
+const rightBtnEl = document.querySelector(".btn-right");
+const leftBtnEl = document.querySelector(".btn-left");
+const stepsEl = document.querySelectorAll(".step");
+const frontLineEl = document.querySelector(".line-front");
+
+let currentChecked = 1;
 
 rightBtnEl.addEventListener("click", () => {
-  const lineEl = document.querySelector(".line");
-  lineEl.classList.add("active");
-  const xmarkEl = document.querySelector(".fa-circle-xmark");
-  xmarkEl.classList.add("active");
+  currentChecked++;
+  if (currentChecked > stepsEl.length) {
+    currentChecked = stepsEl.length;
+  }
+  updateStepProgress();
 });
 
 leftBtnEl.addEventListener("click", () => {
-  const lineEl = document.querySelector(".line");
-  lineEl.classList.remove("active");
-  const xmarkEl = document.querySelector(".fa-circle-xmark");
-  xmarkEl.classList.remove("active");
+  currentChecked--;
+  if (currentChecked < 1) {
+    currentChecked = 1;
+  }
+  updateStepProgress();
 });
+
+function updateStepProgress() {
+  stepsEl.forEach((stepEl, idx) => {
+    if (idx < currentChecked) {
+      stepEl.classList.add("start");
+      stepEl.childNodes[1].classList.add("checked");
+      stepEl.innerHTML = ` <i class="fa-regular fa-circle-check checked"></i>
+        <small>${
+          idx === 0
+            ? "Start"
+            : idx === stepsEl.length - 1
+            ? "Final"
+            : "Step " + idx
+        }</small>`;
+    } else {
+      stepEl.childNodes[1].classList.remove("checked");
+      stepEl.innerHTML = ` <i class="fa-regular fa-circle-xmark"></i>   
+            `;
+    }
+  });
+
+  frontLineEl.style.width =
+    ((currentChecked - 1) / (stepsEl.length - 1)) * 100 + "%";
+
+  if (currentChecked === 1) {
+    leftBtnEl.disabled = true;
+  } else if (currentChecked === stepsEl.length) {
+    rightBtnEl.disabled = true;
+  } else {
+    leftBtnEl.disabled = false;
+    rightBtnEl.disabled = false;
+  }
+}
